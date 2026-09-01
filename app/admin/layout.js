a9b3af3e543e5bf029fca37cd160c3104c4fefeb
Brasiljunior <jed08d@gmail.com>
2026-09-01 11:50:56 -0500

    Add administrator console styling layout

---
+++ app/admin/layout.js
@@ -0,0 +1,5 @@
+import './admin.css';
+
+export const metadata={title:'CompassU Administrator | Control Center',description:'Secure CompassU administrator dashboard.'};
+
+export default function AdminLayout({children}){return children}
