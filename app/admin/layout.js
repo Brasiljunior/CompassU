fc452b2376453cd96741c4da704ad5eae4ef85d4
Brasiljunior <jed08d@gmail.com>
2026-09-07 01:06:01 -0500

    Load institutional analytics styles

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,5 +1,6 @@
 import './admin.css';
 import './batch-guard.css';
+import './analytics.css';
 import BatchInvite500 from './BatchInvite500';
 import AdminForgotPasswordLink from './AdminForgotPasswordLink';
 import AdminSupabaseFetchGuard from './AdminSupabaseFetchGuard';
