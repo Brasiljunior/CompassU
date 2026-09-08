98a313d627aac5afe6b4b8d0681320597595f61d
Brasiljunior <jed08d@gmail.com>
2026-09-07 22:07:07 -0500

    Mount Stage 3B tenant analytics authorization guard

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -5,6 +5,7 @@ import BatchInvite500 from './BatchInvite500';
 import AdminForgotPasswordLink from './AdminForgotPasswordLink';
 import AdminSupabaseFetchGuard from './AdminSupabaseFetchGuard';
 import AdminTenantRoleGate from './AdminTenantRoleGate';
+import AdminTenantAnalyticsGuard from './AdminTenantAnalyticsGuard';
 import AdminInstitutionEnhancer from './AdminInstitutionEnhancer';
 import AdminAnalyticsPanel from './AdminAnalyticsPanel';
 import AdminTrendAnalyticsPanel from './AdminTrendAnalyticsPanel';
@@ -13,4 +14,4 @@ import AdminRoleManagementPanel from './AdminRoleManagementPanel';
 
 export const metadata={title:'CompassU Administrator | Control Center',description:'Secure CompassU administrator dashboard.'};
 
-export default function AdminLayout({children}){return <><AdminSupabaseFetchGuard/><AdminTenantRoleGate/>{children}<AdminAnalyticsPanel/><AdminTrendAnalyticsPanel/><AdminExecutiveInsightsPanel/><AdminForgotPasswordLink/><AdminInstitutionEnhancer/><BatchInvite500/><AdminRoleManagementPanel/></>}
+export default function AdminLayout({children}){return <><AdminSupabaseFetchGuard/><AdminTenantRoleGate/><AdminTenantAnalyticsGuard/>{children}<AdminAnalyticsPanel/><AdminTrendAnalyticsPanel/><AdminExecutiveInsightsPanel/><AdminForgotPasswordLink/><AdminInstitutionEnhancer/><BatchInvite500/><AdminRoleManagementPanel/></>}
