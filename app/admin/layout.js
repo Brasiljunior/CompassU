bf9086fc0a19f26cc9162b95df875b3e377b0337
Brasiljunior <jed08d@gmail.com>
2026-09-11 13:06:25 -0500

    Mount final visible-row institution repair

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -14,6 +14,7 @@ import Admin50kPagingEnhancer from './Admin50kPagingEnhancer';
 import Admin50kOperationsEnhancer from './Admin50kOperationsEnhancer';
 import Admin50kInstitutionFileSync from './Admin50kInstitutionFileSync';
 import Admin50kInstitutionBridge from './Admin50kInstitutionBridge';
+import Admin50kVisibleRowRepair from './Admin50kVisibleRowRepair';
 import AdminActivityTrendRepair from './AdminActivityTrendRepair';
 import AdminInstitutionEnhancer from './AdminInstitutionEnhancer';
 import AdminAnalyticsPanel from './AdminAnalyticsPanel';
@@ -24,4 +25,4 @@ import AdminMonthlySendNowPanel from './AdminMonthlySendNowPanel';
 
 export const metadata={title:'CompassU Administrator | Control Center',description:'Secure CompassU administrator dashboard.'};
 
-export default function AdminLayout({children}){return <><AdminSupabaseFetchGuard/><Admin50kConsoleRouter/><AdminActivityTrendRepair/><Admin50kPagingEnhancer/><Admin50kOperationsEnhancer/><Admin50kInstitutionFileSync/><Admin50kInstitutionBridge/>{children}<AdminAnalyticsPanel/><AdminTrendAnalyticsPanel/><AdminExecutiveInsightsPanel/><AdminMonthlyReportingPanel/><AdminMonthlySendNowPanel/><AdminForgotPasswordLink/><AdminInstitutionEnhancer/><BatchInvite500/></>}
+export default function AdminLayout({children}){return <><AdminSupabaseFetchGuard/><Admin50kConsoleRouter/><AdminActivityTrendRepair/><Admin50kPagingEnhancer/><Admin50kOperationsEnhancer/><Admin50kInstitutionFileSync/><Admin50kInstitutionBridge/>{children}<AdminAnalyticsPanel/><AdminTrendAnalyticsPanel/><AdminExecutiveInsightsPanel/><AdminMonthlyReportingPanel/><AdminMonthlySendNowPanel/><AdminForgotPasswordLink/><AdminInstitutionEnhancer/><Admin50kVisibleRowRepair/><BatchInvite500/></>}
