ad8c7badccd8ef69c7a47f1b46b037efe8d95a05
Brasiljunior <jed08d@gmail.com>
2026-09-17 17:35:09 -0500

    Activate reconciled administrator activity trend repair

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -18,7 +18,8 @@ import Admin50kInstitutionFileSync from './Admin50kInstitutionFileSync';
 import Admin50kInstitutionBridge from './Admin50kInstitutionBridge';
 import Admin50kAccountTableController from './Admin50kAccountTableController';
 import Admin50kVisibleRowRepair from './Admin50kVisibleRowRepair';
+import AdminActivityTrendRepair from './AdminActivityTrendRepair';
 
 export const metadata={title:'CompassU Administrator | Control Center',description:'Secure CompassU administrator dashboard.'};
 
-export default function AdminLayout({children}){return <AdminMfaGate><AdminAccessibilityEnhancer/><AdminSupabaseFetchGuard/><Admin50kConsoleRouter/><Admin50kPagingEnhancer/><Admin50kOperationsEnhancer/><Admin50kInstitutionFileSync/><Admin50kInstitutionBridge/><Admin50kAccountTableController/>{children}<AdminAnalyticsPanel/><AdminTrendAnalyticsPanel/><AdminExecutiveInsightsPanel/><AdminForgotPasswordLink/><AdminInstitutionEnhancer/><Admin50kVisibleRowRepair/><BatchInvite500/></AdminMfaGate>}
+export default function AdminLayout({children}){return <AdminMfaGate><AdminAccessibilityEnhancer/><AdminSupabaseFetchGuard/><Admin50kConsoleRouter/><AdminActivityTrendRepair/><Admin50kPagingEnhancer/><Admin50kOperationsEnhancer/><Admin50kInstitutionFileSync/><Admin50kInstitutionBridge/><Admin50kAccountTableController/>{children}<AdminAnalyticsPanel/><AdminTrendAnalyticsPanel/><AdminExecutiveInsightsPanel/><AdminForgotPasswordLink/><AdminInstitutionEnhancer/><Admin50kVisibleRowRepair/><BatchInvite500/></AdminMfaGate>}
